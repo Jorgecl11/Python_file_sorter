bee193f0b55cbfa76871d57c58af7c36415317d0
Jorgec11 <jlchavez.cs@gmail.com>
2026-02-24 18:56:19 -0800

    added dictionary to sort more than extension and create new folders

--- cleaner.py
+++ cleaner.py
@@ -3,17 +3,24 @@ import shutil # used to move files
 import time #needed for sleep timers
 from datetime import datetime #This imports the datetime library for timestamps
 
+
+FILE_MAP = {
+    ".pdf" : "PDF_Backups",
+    ".docx" : "Word_Backups",
+    ".xlsx" : "Excel_Backups",
+    ".csv" : "Excel_Backups",
+    ".ppt" : "PowerPoint_Backups",
+    ".pptx": "PowerPoint_Backups",
+    ".png" : "Images_backups",
+    ".jpg" : "Images_backups",
+    ".jpeg" : "Images_backups",
+}
 #This finds your 'user' folder automatically.
 home = os.path.expanduser("~")
 folder_path = os.path.join(home, "Downloads")
 #this will create a folder inside downloads to keep it neat.
 target_folder = os.path.join(folder_path, "PowerPoint_Backups")
 log_file = os.path.join(folder_path, "activity_log.txt") # the new log file.
-
-#create backup folder if it doesnt exist yet.
-if not os.path.exists(target_folder):
-    os.makedirs(target_folder)
-    print(f"Created: {target_folder}")
 print(f"Service started. Monitoring {folder_path}... (Ctrl+C to stop.)")
 
 
@@ -24,16 +31,26 @@ while True:
     #'a' appends at the end of the file instead of overwriting it.
     with open(log_file, "a", encoding="utf-8") as log: #utf-8 handles special characters
         for file_name in files:
-            #look for .ppt or .pptx
-            if file_name.lower().endswith((".ppt", ".pptx")):
-                #if any files found this if block executes.
+            name, extension = os.path.splitext(file_name)
+            extension = extension.lower() 
+            
+            if extension in FILE_MAP:
                 files_found = True
+
+                category_folder = FILE_MAP[extension]
+                # DYNAMIC PATH: This changes depending on the file type
+                current_target_path = os.path.join(folder_path,category_folder)
+
+                if not os.path.exists(current_target_path):
+                    os.makedirs(current_target_path)
+                    print(f"Created new category folder: {category_folder}")
 
                 now = datetime.now()
                 #get the current time for the log.
                 timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+
                 old_path = os.path.join(folder_path, file_name)
-                new_path = os.path.join(target_folder, file_name)
+                new_path = os.path.join(current_target_path, file_name)
 
                 #--check for duplicate files--
                 if os.path.exists(new_path):
@@ -41,7 +58,7 @@ while True:
                     name, extension = os.path.splitext(file_name)
                     unique_suffix = now.strftime("Copy_%H%M%S")
                     file_name = f"{name}_{unique_suffix}{extension}"
-                    new_path = os.path.join(target_folder, file_name)
+                    new_path = os.path.join(current_target_path, file_name)
                 #--Duplicate check end--
                 try:
                     shutil.move(old_path, new_path)
@@ -53,7 +70,7 @@ while True:
                 except Exception as e:
                     print(f"Error moving {file_name}: {e}")
     if not files_found:
-        print(f"No PowerPoint files found in {folder_path}.")
+        print(f"[{datetime.now().strftime('%H:%M:%S')}] Scan complete: No matching files found.")
   
 
     time.sleep(60)
